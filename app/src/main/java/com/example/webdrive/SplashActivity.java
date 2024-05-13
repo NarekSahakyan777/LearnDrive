5cd4b1fe9498e00003b96d594f047354e4ad54e5
NarekSahakyan777 <narek.sahakyan.cx@gmail.com>
2024-05-13 14:35:32 +0300

    WebDrive-App

---
+++ app/src/main/java/com/example/webdrive/SplashActivity.java
@@ -0,0 +1,35 @@
+package com.example.webdrive;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.TextView;
+
+public class SplashActivity extends AppCompatActivity {
+    private TextView appName;
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        appName = findViewById(R.id.appName);
+        Animation anim  = AnimationUtils.loadAnimation(this,R.anim.splashanim);
+        appName.setAnimation(anim);
+        new Thread(){
+            @Override
+            public void run(){
+                try {
+                    sleep(4000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        }.start();
+    }
+}
